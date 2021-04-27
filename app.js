edb5de84bc1e5596c71b260c809c91e620f1a500
zeinabqa <zina.qatr@gmail.com>
2021-04-27 22:01:37 +0300

    do

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let username = prompt ('Hello, could you please enter your name ');
+console.log(username);
+console.log(typeof username);
+
+
+let age = prompt ('Hello, could you please enter your name '  );
+console .log(age);
+console.log( typeof age );
+
+//Iwant to change the prompt data from string to number
+age=Number(age);
+console.log(age);
+console.log(typeof age);
+console.log(age + 12);
+
+if (age ===18 ){
+    alert('you are 18');
+}else if (age > 18 ){
+    alert('you are welcome');
+}else{
+    alert('you cant b/c your are too young');
+
+}
+
+ case true :
+
+document.write('have a nice day ');
+break;
+ 
+case false 
+ document.write('do you write homework ');
+ break;
+
+
+
+
+
+
